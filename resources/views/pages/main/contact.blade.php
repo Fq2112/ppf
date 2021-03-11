fc9ba3915dd9fbdf9ab7d3f5b66b4cda2895b237
Fq2112 <fiqy_a@icloud.com>
2021-03-11 12:21:58 +0700

    reCAPTCHA contact form

--- resources/views/pages/main/contact.blade.php
+++ resources/views/pages/main/contact.blade.php
@@ -5,7 +5,22 @@
         .banner-2{
             background: url({{asset('images/banner/contact.jpg')}});
         }
+
+        .contact_form input[type=submit] {
+            margin-top: 0;
+            padding: 1.6em 3em;
+        }
+
+        .contact_form input:disabled {
+            opacity: .8 !important;
+            cursor: no-drop;
+        }
+
+        #recaptcha-contact {
+            float: right;
+        }
     </style>
+    <script src='https://www.google.com/recaptcha/api.js?onload=recaptchaCallback&render=explicit' async defer></script>
 @endpush
 @section('content')
     <!-- Contact -->
@@ -16,7 +31,7 @@
             <div class="w3-agileitsline"></div>
             <form action="{{route('submit.contact')}}" method="post" class="contact_form">
                 @csrf
-                <div class="message">
+                <div class="row message">
                     <div data-aos="fade-right" class="col-md-6 col-sm-6 grid_6 c1">
                         <input type="text" name="name" class="text" placeholder="Full name" required>
                         <input type="email" name="email" class="email" placeholder="Email" required>
@@ -25,8 +40,14 @@
                     <div data-aos="fade-left" class="col-md-6 col-sm-6 grid_6 c1">
                         <textarea name="message" placeholder="Write something here..." required></textarea>
                     </div>
-                    <div class="clearfix"></div>
-                    <input data-aos="fade-down" type="submit" name="send" class="more_btn" value="Send Message">
+                </div>
+                <div class="row message" data-aos="fade-down">
+                    <div class="col-md-6 col-sm-6">
+                        <div id="recaptcha-contact"></div>
+                    </div>
+                    <div class="col-md-6 col-sm-6 text-left">
+                        <input data-aos="fade-down" type="submit" name="send" class="more_btn" value="SEND MESSAGE" disabled>
+                    </div>
                 </div>
             </form>
         </div>
@@ -39,6 +60,30 @@
 @endsection
 @push('scripts')
     <script>
+        var recaptcha_contact, recaptchaCallback = function () {
+            recaptcha_contact = grecaptcha.render(document.getElementById('recaptcha-contact'), {
+                'sitekey': '{{env('reCAPTCHA_v2_SITEKEY')}}',
+                'callback': 'enable_btnContact',
+                'expired-callback': 'disabled_btnContact'
+            });
+        };
+
+        function enable_btnContact() {
+            $(".more_btn").removeAttr('disabled');
+        }
+
+        function disabled_btnContact() {
+            $(".more_btn").attr('disabled', 'disabled');
+        }
+
+        $(".contact_form").on("submit", function (e) {
+            if (grecaptcha.getResponse(recaptcha_contact).length === 0) {
+                e.preventDefault();
+                swal('ATTENTION!', 'Please confirm us that you\'re not a robot by clicking in ' +
+                    'the reCAPTCHA dialog-box.', 'warning');
+            }
+        });
+
         @if(session('contact'))
         swal('Successfully sent a message!', '{{ session('contact') }}', 'success');
         @endif
